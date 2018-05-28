2fc3ec27e4a4af5d7c64c90b5ce64dba2f73dcb1
Trey Mosley <iiimosley@gmail.com>
2018-05-27 23:03:33 -0500

    rotate on home text

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,40 @@
 <template>
 <div>
-  <h2>You are Home</h2>
+  <h2>
+    <span v-for="(letter, i) in text" :key="`letter${i}`" :id="`letter${i}`">{{letter}}</span>
+  </h2>
 </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      text: 'You Are Home',
+      fontColor: 'blue'
+    }
+  },
+  mounted() {
+    for(let i=0; i<this.text.length; i++){
+      document.querySelector(`#letter${i}`).style.transform = `rotate(${360/this.text.length*i}deg)`;
+    }
+  },
 }
 </script>
 
-<style scoped>
-  
+<style lang="scss" scoped>
+h2 {
+  position: relative;
+  text-align: center;
+  span{
+    height: 3em;
+    position: absolute;
+    width: 20px;
+    left: 0;
+    top: 0;
+    transform-origin: bottom center;
+  }
+}
+
+
 </style>
