a0ca2d483351a0050e4d8b1657ebf49bf8e1f14d
Trey Mosley <iiimosley@gmail.com>
2018-05-28 16:33:14 -0500

    repositioning + antialiasing

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -18,33 +18,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-p {
-  width: 30%;
-  margin: 3em auto;
-  position: relative;
-  text-align: center;
-  animation: scaleText 2s alternate infinite;
-  span {
-    font-size: 3em;
-    height: 4em;
-    position: absolute;
-    width: 1em;
-    left: 0;
-    top: 0;
-    transform-origin: bottom center;
-  }
-  @for $i from 0 through 12 {
-    $start: 360/13*$i;
-    $end: 360/13*$i+360;
-    #letter#{$i} {
-      animation: roundAbout#{$i} 6s infinite linear;
+div{
+  width: 70%;
+  p {
+    display: block;
+    width: 30%;
+    margin: auto;
+    position: relative;
+    transform-origin: center;
+    animation: scaleText 3s alternate infinite linear;
+    span {
+      font-size: 3em;
+      height: 3em;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform-origin: bottom center;
     }
-    @keyframes roundAbout#{$i} {
-      from {
-        transform: rotate(($start)+deg);
+    @for $i from 0 through 12 {
+      $start: 360/13*$i;
+      $end: 360/13*$i+360;
+      #letter#{$i} {
+        animation: roundAbout#{$i} 6s infinite linear;
       }
-      to {
-        transform: rotate(($end)+deg);
+      @keyframes roundAbout#{$i} {
+        from {
+          transform: rotate(($start)+deg);
+        }
+        to {
+          transform: rotate(($end)+deg);
+        }
       }
     }
   }
@@ -52,7 +55,8 @@ p {
 
 @keyframes scaleText {
   to {
-    transform: scale(1.3);
+    transform: scale(1.5);
+    text-shadow: 0 0 1em rgb(37, 32, 37);
   }
 }
 
